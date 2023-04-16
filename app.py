b3c2d4479edb602c39124e46a6814cf4e4eb8f1e
Eric Julianto <algonacci1@gmail.com>
2023-04-16 17:21:22 +0700

    Finishing

--- app.py
+++ app.py
@@ -1,47 +1,84 @@
-from flask import Flask, request, jsonify
+import csv
+import datetime
+import os
+import uuid
+
+import cv2
+import pandas as pd
+import torch
+from flask import Flask, render_template, request
 from google.cloud import vision
+from google.oauth2.service_account import Credentials
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+creds = Credentials.from_service_account_file('credentials2.json')
+client = vision.ImageAnnotatorClient(credentials=creds)
 
 
-@app.route("/")
+now = datetime.datetime.now()
+timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello World!"
-
-@app.route('/detect-ocr', methods=['POST'])
-def detect_ocr():
-    # Get the image file from the request
-    image_file = request.files.get('image')
-    
-    # Create a client for the Google Cloud Vision API
-    client = vision.ImageAnnotatorClient()
-    
-    # Read the contents of the image file
-    image_content = image_file.read()
-    
-    # Create an image object
-    image = vision.Image(content=image_content)
-    
-    # Configure the OCR feature
-    ocr_feature = vision.Feature(type_=vision.Feature.Type.TEXT_DETECTION)
-    
-    # Perform text detection on the image
-    response = client.annotate_image({
-        'image': image,
-        'features': [ocr_feature],
-    })
-    
-    # Get the text annotations from the response
-    annotations = response.text_annotations
-    
-    # Return the first annotation (which should contain the entire text)
-    if annotations:
-        text = annotations[0].description
-        return jsonify({'text': text})
-    
-    # If no text annotations were found, return an error message
+    if request.method == "POST":
+        image = request.files["image"]
+        if image and allowed_file(image.filename):
+            image.save(os.path.join(
+                app.config['UPLOAD_FOLDER'], secure_filename(image.filename)))
+            image_path = os.path.join(
+                app.config['UPLOAD_FOLDER'], secure_filename(image.filename))
+            model = torch.hub.load("ultralytics/yolov5",
+                                   "custom",
+                                   path="./best.pt",
+                                   force_reload=True)
+            result = model(image_path)
+            print(result)
+            bbox_raw = result.xyxy[0][0]
+            bbox = []
+            for bound in bbox_raw:
+                bbox.append(int(bound.item()))
+            bbox = bbox[:4]
+            image = cv2.imread(image_path)
+            cropped_image = image[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+            dynamic_filename = str(uuid.uuid4())
+            cv2.imwrite("static/result/cropped_image_" +
+                        dynamic_filename + ".jpg", cropped_image)
+            cropped_image_path = "static/result/cropped_image_" + dynamic_filename + ".jpg"
+            with open(cropped_image_path, 'rb') as f:
+                image_bytes = f.read()
+            image = vision.Image(content=image_bytes)
+            response = client.text_detection(image=image)
+            text = response.text_annotations[0].description
+            row = {'timestamp': timestamp,
+                   'uploaded_image_path': image_path,
+                   'cropped_image_path': cropped_image_path,
+                   'result_text': text}
+            fields = ["timestamp", "uploaded_image_path",
+                      "cropped_image_path", "result_text"]
+            with open('ocr_results.csv', 'a') as f:
+                writer = csv.DictWriter(f, fieldnames=fields)
+                writer.writerow(row)
+            return render_template("index.html", result=cropped_image_path, water_meter=text)
+        else:
+            return render_template("index.html", error="Silahkan upload gambar dengan format JPG")
     else:
-        return jsonify({'error': 'No text found in the image'})
+        return render_template("index.html")
+
+
+@app.route("/history")
+def history():
+    df = pd.read_csv("ocr_results.csv")
+    df['result_text'] = df['result_text'].astype(str).str.zfill(7)
+    return render_template("history.html", df=df)
 
 
 if __name__ == "__main__":
